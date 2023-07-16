855c3509b0ea688b88b37698b308c77f006c885b
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-07-16 19:46:57 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Oleg Gavashi on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
